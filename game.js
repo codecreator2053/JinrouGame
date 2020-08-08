ffac0d09669a57bcebcacce1430e765d85d1dbcc
Vipul Mishra <vipulmishra@h29is191.naist.jp>
2020-08-09 06:12:24 +0900

    added interactive feture to game.html

--- game.js
+++ game.js
@@ -6,7 +6,23 @@ function changeColor(){
 	console.log(typeof(timer));
 }
 
+function randColor(){
+	code = '#' + addColorCode('');
+	console.log(code);
+	document.body.style.background = code;
+}
 
+function addColorCode(cur_code){
+	var num = [0,1,2,3,4,5,6,7,8,9,'a','b','c','d','e','f'];
+	one_code = num[Math.floor(Math.random()*16)];
+	cur_code+=one_code;
+	if (cur_code.length==6){
+		return cur_code; 
+	}
+	else{
+		return addColorCode(cur_code);
+	}
+}
 
 let chg_btn = document.getElementById("change");
 chg_btn.onclick = changeColor;
@@ -21,22 +37,6 @@ function stopChange(){
 let stop_btn =  document.getElementById("stop");
 stop_btn.onclick = stopChange;
 
-class ColorCreator{
-	function randColor(){
-		code = '#' + addColorCode('');
-		console.log(code);
-		document.body.style.background = code;
-	}
 
-	function addColorCode(cur_code){
-		var num = [0,1,2,3,4,5,6,7,8,9,'a','b','c','d','e','f'];
-		one_code = num[Math.floor(Math.random()*16)];
-		cur_code+=one_code;
-		if (cur_code.length==6){
-			return cur_code; 
-		}
-		else{
-			return addColorCode(cur_code);
-		}
-	}
-}
+
+
