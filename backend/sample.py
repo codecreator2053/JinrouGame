7fe27eb9afe80a8efde045b9c90b4f24f326bb5f
氏家翔吾 <suzzz428@gmail.com>
2020-08-08 22:47:49 +0900

    add init change

--- backend/sample.py
+++ backend/sample.py
@@ -1,7 +1,14 @@
+from pathlib import Path
+
 from flask import Flask, render_template
 
-app = Flask(__name__)
+static_folder = Path.cwd()
+
+app = Flask(__name__, template_folder=str(static_folder.parents[0]), static_folder=str(static_folder.parents[0] / "resources"))
 
 @app.route("/")
 def main():
-    return render_template()
+    return render_template("home.html")
+
+if __name__ == "__main__":
+    app.run()
