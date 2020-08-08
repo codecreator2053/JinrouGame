3355b9c4260876de6eb3043baf65d905b655c767
氏家翔吾 <suzzz428@gmail.com>
2020-08-08 23:04:22 +0900

    add session

--- backend/sample.py
+++ backend/sample.py
@@ -1,14 +1,35 @@
 from pathlib import Path
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for, session
 
 static_folder = Path.cwd()
 
 app = Flask(__name__, template_folder=str(static_folder.parents[0]), static_folder=str(static_folder.parents[0] / "resources"))
 
+app.secret_key = 'A0Zr9foijaoe090'
+
 @app.route("/")
 def main():
-    return render_template("home.html")
+    if "username" in session:
+        print("Hello " + str(session['username']))
+        return render_template('home.html')
+    return redirect(url_for('login'))
+
+
+@app.route('/login', methods=['POST'])
+def login():
+    session['username'] = request.form['username']
+    return redirect(url_for('main'))
+
+
+@app.route('/login', methods=['GET'])
+def login_view():
+    return """
+        <form action="" method="post">
+            <p><input type=text name=username>
+            <p><input type=submit value=Login>
+        </form>
+    """
 
 if __name__ == "__main__":
     app.run()
